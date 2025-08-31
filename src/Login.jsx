b2d35de1d5f41e3f18d17a0bd7b47b0feef089f5
Venkata-Subrahmanyam-2005 <venkatsubbu808@gmail.com>
2025-08-31 14:39:38 +0530

    create routes

---
+++ src/Login.jsx
@@ -0,0 +1,10 @@
+const Login=()=>
+{
+    return (
+        <div className="m-5 text-3xl font-bold">
+            Login
+        </div>
+    )
+}
+
+export default Login;
---
+++ src/Profile.jsx
@@ -0,0 +1,9 @@
+const Profile=()=>{
+    return (
+        <div className="m-5 text-3xl font-bold">
+            Profile
+        </div>
+    )
+}
+
+export default Profile;
